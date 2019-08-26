92c4c469ed7a084b6c71fbec09529e29409289a5
Ivan Kulaha <i.kulaga@itransition.com>
2019-08-26 20:41:26 +0300

    Simple grid 20x20 is added.

--- src/Cell.js
+++ src/Cell.js
@@ -2,16 +2,30 @@ import React from 'react';
 
 
 class Cell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelected: false
+        }
+    }
+
     render() {
         var style = {
             width: 20,
             height: 20,
             border: "1px solid #AEC1FF",
             borderRadius: "2px",
+            backgroundColor: this.state.isSelected ? "#91C5FF" : "white",
             margin: "1px"
         };
 
-        return <div style={style}></div>
+        return <div style={style} onClick={() => this.switchSelection()}></div>
+    }
+
+    switchSelection() {
+        this.setState({
+            isSelected: !this.state.isSelected
+        });
     }
 }
 
