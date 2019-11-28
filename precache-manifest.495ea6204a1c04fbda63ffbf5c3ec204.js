7a1aea25803808982443b84d3e87179c2d617023
Ivan Kulaha <ivkulaga@gmail.com>
2019-11-28 23:43:20 +0300

    Updates

---
+++ precache-manifest.495ea6204a1c04fbda63ffbf5c3ec204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cba3992be016552acbc3d8e6cd2b71c",
+    "url": "/Life2D/index.html"
+  },
+  {
+    "revision": "7514ba310e644a03e414",
+    "url": "/Life2D/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9007d8befe46310d598e",
+    "url": "/Life2D/static/css/main.9e2622c9.chunk.css"
+  },
+  {
+    "revision": "7514ba310e644a03e414",
+    "url": "/Life2D/static/js/2.75dd8997.chunk.js"
+  },
+  {
+    "revision": "9007d8befe46310d598e",
+    "url": "/Life2D/static/js/main.961e6c56.chunk.js"
+  },
+  {
+    "revision": "adeaa261d2b4c74be301",
+    "url": "/Life2D/static/js/runtime~main.87c59c91.js"
+  }
+]);
